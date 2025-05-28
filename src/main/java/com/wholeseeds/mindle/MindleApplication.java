16558cac66e1a835cfce8434e5125d30afcd1347
chanwoo7 <chanwoo9987@gmail.com>
2025-05-29 01:02:16 +0900

    Initialize Spring Boot Project

---
+++ src/main/java/com/wholeseeds/mindle/MindleApplication.java
@@ -0,0 +1,13 @@
+package com.wholeseeds.mindle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wholeseeds/mindle/MindleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wholeseeds.mindle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
