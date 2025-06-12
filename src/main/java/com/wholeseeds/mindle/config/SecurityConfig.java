18e2fd12376bb58d69304eb57613e2eec6972996
chanwoo7 <chanwoo9987@gmail.com>
2025-06-13 01:11:50 +0900

    chore: Spring Security 복구, Swagger 경로 접근 허용

---
+++ src/main/java/com/wholeseeds/mindle/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.wholeseeds.mindle.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(AbstractHttpConfigurer::disable)  // 테스트 목적으로 해제
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers(
+					"/swagger-ui/**", "/v3/api-docs/**"
+				).permitAll()
+				.anyRequest().authenticated()
+			);
+
+		return http.build();
+	}
+}
