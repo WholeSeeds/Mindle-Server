bdf98077d8d0b279116fc11b4e56e7909b585f15
chanwoo7 <chanwoo9987@gmail.com>
2025-06-16 03:00:25 +0900

    feat: #1 Member repository 추가, 서비스 계층 사용 예시 추가

---
+++ src/main/java/com/wholeseeds/mindle/domain/member/repository/MemberRepositoryCustom.java
@@ -0,0 +1,12 @@
+package com.wholeseeds.mindle.domain.member.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.wholeseeds.mindle.domain.member.entity.Member;
+
+public interface MemberRepositoryCustom {
+	Optional<Member> findByIdNotDeleted(Long id);
+
+	List<Member> findAllNotDeleted();
+}
