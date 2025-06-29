814fbbc729f7edf403449b489d398beeff381d50
chanwoo7 <chanwoo9987@gmail.com>
2025-06-29 22:19:09 +0900

    refactor: #7 Firebase 계정 설정파일 경로 하드코딩 제거

--- src/main/java/com/wholeseeds/mindle/config/FirebaseConfig.java
+++ src/main/java/com/wholeseeds/mindle/config/FirebaseConfig.java
@@ -1,9 +1,11 @@
 package com.wholeseeds.mindle.config;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
@@ -14,16 +16,19 @@ import jakarta.annotation.PostConstruct;
 @Configuration
 public class FirebaseConfig {
 
+	@Value("${firebase.config-path}")
+	private Resource firebaseConfigFile;
+
 	@PostConstruct
 	public void initialize() throws IOException {
-		FileInputStream serviceAccount =
-			new FileInputStream("mindle-secret.json");
+		try (InputStream serviceAccount = firebaseConfigFile.getInputStream()) {
+			FirebaseOptions options = FirebaseOptions.builder()
+				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+				.build();
 
-		FirebaseOptions options = FirebaseOptions.builder()
-			.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-			.build();
-
-		FirebaseApp.initializeApp(options);
+			if (FirebaseApp.getApps().isEmpty()) {
+				FirebaseApp.initializeApp(options);
+			}
+		}
 	}
 }
-
