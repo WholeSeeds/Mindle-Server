2cf33ccb4bb304aca6c4dce8f285659aa0b2845a
chanwoo7 <chanwoo9987@gmail.com>
2025-07-03 02:35:58 +0900

    hotfix: #12 Spring Data JPA 전용 생성자 추가

--- src/main/java/com/wholeseeds/mindle/common/repository/JpaBaseRepositoryImpl.java
+++ src/main/java/com/wholeseeds/mindle/common/repository/JpaBaseRepositoryImpl.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.support.JpaEntityInformation;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 
 import com.querydsl.core.types.EntityPath;
@@ -34,6 +35,19 @@ public class JpaBaseRepositoryImpl<T extends BaseEntity, ID extends Serializable
 	protected final NumberPath<Long> idPath;
 	protected final DateTimePath<?> deletedAtPath;
 
+	// Spring Data JPA가 사용하는 생성자
+	public JpaBaseRepositoryImpl(JpaEntityInformation<T, ?> entityInformation, EntityManager em) {
+		super(entityInformation, em);
+		this.em = em;
+		this.queryFactory = new JPAQueryFactory(em);
+
+		// 이 3개는 null 처리해도 되고, 하위 클래스에서 재정의할 수 있음
+		this.entityPath = null;
+		this.idPath = null;
+		this.deletedAtPath = null;
+	}
+
+	// 커스텀용 생성자 (직접 구현할 때 사용)
 	public JpaBaseRepositoryImpl(Class<T> domainClass, EntityManager em, EntityPath<T> entityPath,
 		NumberPath<Long> idPath, DateTimePath<?> deletedAtPath) {
 		super(domainClass, em);
