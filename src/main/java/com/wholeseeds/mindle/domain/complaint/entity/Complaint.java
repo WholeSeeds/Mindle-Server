806a2012ea184e3e391ed9033f09427decc6e431
hyeesw <hyeeun7904@gmail.com>
2025-07-03 10:40:02 +0900

    refactor : complaint 필드 기본값

--- src/main/java/com/wholeseeds/mindle/domain/complaint/entity/Complaint.java
+++ src/main/java/com/wholeseeds/mindle/domain/complaint/entity/Complaint.java
@@ -57,14 +57,25 @@ public class Complaint extends BaseEntity {
 
 	@Enumerated(EnumType.STRING)
 	@Column
-	private Status status = Status.REPORTED;
+	private Status status;
 
 	@Column(name = "is_resolved")
-	private Boolean isResolved = false;
+	private Boolean isResolved;
 
 	public enum Status {
 		REPORTED,
 		IN_PROGRESS,
 		RESOLVED
+	}
+
+	// 초기값
+	@Override
+	protected void onPrePersist() {
+		if (status == null) {
+			status = Status.REPORTED;
+		}
+		if (isResolved == null) {
+			isResolved = false;
+		}
 	}
 }
