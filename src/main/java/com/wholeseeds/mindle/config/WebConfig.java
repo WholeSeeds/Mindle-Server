d34e93212c836f1e127699c2b69a2e93085752fe
chanwoo7 <chanwoo9987@gmail.com>
2025-07-06 03:55:35 +0900

    feat: #16 Logging 인터셉터, WebConfig 추가

---
+++ src/main/java/com/wholeseeds/mindle/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.wholeseeds.mindle.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.wholeseeds.mindle.global.interceptor.LoggingInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	private final LoggingInterceptor loggingInterceptor;
+
+	public WebConfig(LoggingInterceptor loggingInterceptor) {
+		this.loggingInterceptor = loggingInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(loggingInterceptor);
+	}
+}
