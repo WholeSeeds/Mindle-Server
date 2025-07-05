165c81ddca64fefc8fb7124529c7555df7b61640
chanwoo7 <chanwoo9987@gmail.com>
2025-07-06 03:59:35 +0900

    feat: #16 RequestLogger, ResponseTemplate 추가

--- src/main/java/com/wholeseeds/mindle/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/wholeseeds/mindle/global/exception/GlobalExceptionHandler.java
@@ -1,14 +1,18 @@
 package com.wholeseeds.mindle.global.exception;
 
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.wholeseeds.mindle.common.exception.BusinessException;
 import com.wholeseeds.mindle.common.response.ErrorCode;
-import com.wholeseeds.mindle.common.response.ErrorResponse;
+import com.wholeseeds.mindle.global.util.ResponseTemplate;
 
 import io.swagger.v3.oas.annotations.Hidden;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -16,41 +20,37 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Hidden
 @RestControllerAdvice
+@RequiredArgsConstructor
 @Slf4j
 public class GlobalExceptionHandler {
+	private final ResponseTemplate responseTemplate;
 
 	/**
 	 * 비즈니스 로직에서 발생한 커스텀 예외 처리
 	 */
 	@SuppressWarnings("checkstyle:ParameterName")
 	@ExceptionHandler(BusinessException.class)
-	public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException ex) {
+	public ResponseEntity<Map<String, Object>> handleBusinessException(BusinessException ex) {
 		ErrorCode errorCode = ex.getErrorCode();
 		log.warn("[BusinessException] {} - {}", errorCode.name(), ex.getMessage());
-		return ResponseEntity
-			.status(errorCode.toHttpStatus())
-			.body(ErrorResponse.of(errorCode));
+		return responseTemplate.fail(ex, errorCode.toHttpStatus());
 	}
 
 	/**
 	 * 잘못된 요청 파라미터나 기본 예외 처리
 	 */
 	@ExceptionHandler(IllegalArgumentException.class)
-	public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+	public ResponseEntity<Map<String, Object>> handleIllegalArgumentException(IllegalArgumentException ex) {
 		log.warn("[IllegalArgumentException] {}", ex.getMessage(), ex);
-		return ResponseEntity
-			.badRequest()
-			.body(ErrorResponse.of(ErrorCode.BAD_REQUEST));
+		return responseTemplate.fail(ex, HttpStatus.BAD_REQUEST);
 	}
 
 	/**
 	 * 예상하지 못한 예외 (catch 되지 않은 나머지)
 	 */
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+	public ResponseEntity<Map<String, Object>> handleException(Exception ex) {
 		log.error("[Unhandled Exception]", ex);
-		return ResponseEntity
-			.internalServerError()
-			.body(ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR));
+		return responseTemplate.fail(ex, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
