1c36c18f664bb3cb14d3c04252e5cbe2661c0e91
chanwoo7 <chanwoo9987@gmail.com>
2025-07-10 00:38:36 +0900

    fix: #15 FirebaseAuthInterceptor 인터셉터 등록

--- src/main/java/com/wholeseeds/mindle/config/WebConfig.java
+++ src/main/java/com/wholeseeds/mindle/config/WebConfig.java
@@ -4,19 +4,29 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.wholeseeds.mindle.global.interceptor.FirebaseAuthInterceptor;
 import com.wholeseeds.mindle.global.interceptor.LoggingInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
 	private final LoggingInterceptor loggingInterceptor;
+	private final FirebaseAuthInterceptor firebaseAuthInterceptor;
 
-	public WebConfig(LoggingInterceptor loggingInterceptor) {
+	public WebConfig(
+		LoggingInterceptor loggingInterceptor,
+		FirebaseAuthInterceptor firebaseAuthInterceptor
+	) {
 		this.loggingInterceptor = loggingInterceptor;
+		this.firebaseAuthInterceptor = firebaseAuthInterceptor;
 	}
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(loggingInterceptor);
+		registry.addInterceptor(loggingInterceptor)
+			.addPathPatterns("/**");
+
+		registry.addInterceptor(firebaseAuthInterceptor)
+			.addPathPatterns("/api/**"); // 또는 /** 로 전체 요청 적용 가능
 	}
 }
