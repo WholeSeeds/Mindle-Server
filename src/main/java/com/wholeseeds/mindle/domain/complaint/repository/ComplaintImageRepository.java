8dd3edcd64ee091d2fa7f2d0bc7643292746c089
chanwoo7 <chanwoo9987@gmail.com>
2025-07-12 10:40:12 +0900

    chore: #15 JpaBaseRepository 상속으로 변경

--- src/main/java/com/wholeseeds/mindle/domain/complaint/repository/ComplaintImageRepository.java
+++ src/main/java/com/wholeseeds/mindle/domain/complaint/repository/ComplaintImageRepository.java
@@ -1,8 +1,7 @@
 package com.wholeseeds.mindle.domain.complaint.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.wholeseeds.mindle.common.repository.JpaBaseRepository;
 import com.wholeseeds.mindle.domain.complaint.entity.ComplaintImage;
 
-public interface ComplaintImageRepository extends JpaRepository<ComplaintImage, Long> {
+public interface ComplaintImageRepository extends JpaBaseRepository<ComplaintImage, Long> {
 }
