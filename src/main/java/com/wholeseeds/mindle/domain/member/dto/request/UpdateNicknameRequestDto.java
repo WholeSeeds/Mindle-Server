e1184e68a3fa9e99b6b7e8bbc8b4745e442e902a
chanwoo7 <chanwoo9987@gmail.com>
2025-08-10 10:03:12 +0900

    refactor: #45 DTO 애노테이션 정리

--- src/main/java/com/wholeseeds/mindle/domain/member/dto/request/UpdateNicknameRequestDto.java
+++ src/main/java/com/wholeseeds/mindle/domain/member/dto/request/UpdateNicknameRequestDto.java
@@ -2,11 +2,13 @@ package com.wholeseeds.mindle.domain.member.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class UpdateNicknameRequestDto {
 
 	@NotBlank(message = "닉네임은 필수입니다.")
--- src/main/java/com/wholeseeds/mindle/domain/member/dto/request/UpdateSubdistrictRequestDto.java
+++ src/main/java/com/wholeseeds/mindle/domain/member/dto/request/UpdateSubdistrictRequestDto.java
@@ -1,11 +1,13 @@
 package com.wholeseeds.mindle.domain.member.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class UpdateSubdistrictRequestDto {
 
 	@NotNull(message = "subdistrictCode는 필수입니다.")
