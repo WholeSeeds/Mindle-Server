37c54487a1781e288c710845ca16415bc512bfbb
hyeesw <hyeeun7904@gmail.com>
2025-08-10 23:58:06 +0900

    fix : /actuator/health/ 경로 접근 허용 (health check)

--- src/main/java/com/wholeseeds/mindle/config/SecurityConfig.java
+++ src/main/java/com/wholeseeds/mindle/config/SecurityConfig.java
@@ -18,7 +18,8 @@ public class SecurityConfig {
 				.requestMatchers(
 					"/swagger-ui/**",
 					"/v3/api-docs/**",
-					"/api/**"
+					"/api/**",
+					"/actuator/health", "/actuator/health/**"
 				).permitAll()
 				.anyRequest().authenticated()
 			);
