f56089cbdc2784eef68e486c74b0b64085169c9a
hyeesw <hyeeun7904@gmail.com>
2025-08-11 00:20:14 +0900

    fix : health check 삭제

--- src/main/java/com/wholeseeds/mindle/config/SecurityConfig.java
+++ src/main/java/com/wholeseeds/mindle/config/SecurityConfig.java
@@ -18,8 +18,8 @@ public class SecurityConfig {
 				.requestMatchers(
 					"/swagger-ui/**",
 					"/v3/api-docs/**",
-					"/api/**",
-					"/actuator/health", "/actuator/health/**"
+					"/api/**"
+					// "/actuator/health", "/actuator/health/**"
 				).permitAll()
 				.anyRequest().authenticated()
 			);
