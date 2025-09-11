e18ea4ba40a227165a86a0567638bf99d5dd406e
chanwoo7 <chanwoo9987@gmail.com>
2025-09-12 03:26:38 +0900

    fix: #32 민원 응답 DTO 내 placeId -> PlaceDto로 변경

--- src/main/java/com/wholeseeds/mindle/domain/complaint/dto/response/SaveComplaintResponseDto.java
+++ src/main/java/com/wholeseeds/mindle/domain/complaint/dto/response/SaveComplaintResponseDto.java
@@ -1,6 +1,7 @@
 package com.wholeseeds.mindle.domain.complaint.dto.response;
 
 import com.wholeseeds.mindle.domain.complaint.entity.Complaint;
+import com.wholeseeds.mindle.domain.place.dto.PlaceDto;
 import com.wholeseeds.mindle.domain.region.dto.SubdistrictDto;
 
 import lombok.AllArgsConstructor;
@@ -16,7 +17,7 @@ public class SaveComplaintResponseDto {
 	private Long categoryId;
 	private Long memberId;
 	private SubdistrictDto subdistrictDto;
-	private Long placeId;
+	private PlaceDto placeDto;
 	private String title;
 	private String content;
 	private Double latitude;
--- src/main/java/com/wholeseeds/mindle/domain/complaint/mapper/ComplaintMapper.java
+++ src/main/java/com/wholeseeds/mindle/domain/complaint/mapper/ComplaintMapper.java
@@ -9,14 +9,15 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
 import com.wholeseeds.mindle.domain.complaint.dto.request.UpdateComplaintRequestDto;
 import com.wholeseeds.mindle.domain.complaint.dto.response.SaveComplaintResponseDto;
 import com.wholeseeds.mindle.domain.complaint.entity.Complaint;
+import com.wholeseeds.mindle.domain.place.mapper.PlaceMapper;
 import com.wholeseeds.mindle.domain.region.mapper.RegionMapper;
 
-@Mapper(componentModel = "spring", uses = {RegionMapper.class})
+@Mapper(componentModel = "spring", uses = {RegionMapper.class, PlaceMapper.class})
 public interface ComplaintMapper {
 	@Mapping(target = "categoryId", source = "category.id")
 	@Mapping(target = "memberId", source = "member.id")
 	@Mapping(target = "subdistrictDto", source = "subdistrict")
-	@Mapping(target = "placeId", source = "place.id")
+	@Mapping(target = "placeDto", source = "place")
 	SaveComplaintResponseDto toSaveComplaintResponseDto(Complaint complaint);
 
 	@BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
